83a53be0cf6382b431fa976dc8963e45d5202eca
Haya Pirzada <hypxada@gmail.com>
2026-01-14 01:30:15 -0500

    uploading project

---
+++ js/buttons.js
@@ -0,0 +1,11 @@
+function goHome() {
+  window.location.href = "index.html";
+}
+
+function goProjects() {
+  window.location.href = "projects.html";
+}
+
+function goHobbies() {
+  window.location.href = "hobbies.html";
+}
